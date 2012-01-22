f2e9b59ef55d83e6add735f7ce08b3441d36a485
Eric Willisson <ericwillisson@gmail.com>
2012-01-22 14:38:58 -0500

    Put in records of factor.c

--- factor.c
+++ factor.c
@@ -1,3 +1,27 @@
+/* factor -- return prime factors of n.
+   Copyright (C) 2012, Eric Willisson <ericwillisson@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Based on factor.c from "coreutils", with original information:
+   
+   Written by Paul Rubin <phr@ocf.berkeley.edu>.
+   Adapted for GNU, fixed to factor UINT_MAX by Jim Meyering.
+   Arbitrary-precision code adapted by James Youngman from Torbjörn
+   Granlund's factorize.c, from GNU MP version 4.2.2.
+*/
+
 #include <Python.h>
 #include <gmp.h>
 
